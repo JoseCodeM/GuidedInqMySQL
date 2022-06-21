deff37761e02a29f1c1957a0d55f9547a59cf97f
Jose Martinez <63440483+JoseCodeM@users.noreply.github.com>
2022-06-20 23:18:44 -0400

    Update Hard.sql

--- Hard.sql
+++ Hard.sql
@@ -1,34 +1,34 @@
 SELECT * FROM guidedInquiry.hard;
 
--- CREATE TABLE hard LIKE Medium
+CREATE TABLE hard LIKE Medium
 
--- INSERT INTO hard SELECT * FROM Medium
+INSERT INTO hard SELECT * FROM Medium
 
--- ALTER TABLE hard 
--- 	ADD FirstName varchar(40) NOT NULL
--- 		AFTER Rating;
+ALTER TABLE hard 
+	ADD FirstName varchar(40) NOT NULL
+		AFTER Rating;
 
--- ALTER TABLE hard 
--- 	ADD LastName varchar(40) NOT NULL
--- 		AFTER FirstName;
+ALTER TABLE hard 
+	ADD LastName varchar(40) NOT NULL
+		AFTER FirstName;
 
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Michael', `LastName` = 'Bay' WHERE (`Title` = 'Armageddon ');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Michael', `LastName` = 'Bay' WHERE (`Title` = 'Bad Boys');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Christopher', `LastName` = 'Nolan' WHERE (`Title` = 'Interstellar');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Oran', `LastName` = 'Peli' WHERE (`Title` = 'Paranormal Activity');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Keenan', `LastName` = 'Wayans' WHERE (`Title` = 'Scary Movie 1');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Keenan', `LastName` = 'Wayans' WHERE (`Title` = 'Scary Movie 2');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Vicky', `LastName` = 'Jenson' WHERE (`Title` = 'Shrek');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Sam', `LastName` = 'Raimi' WHERE (`Title` = 'Spider-Man 3');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Tobe', `LastName` = 'Hooper' WHERE (`Title` = 'Texas Chainsaw Massacre');
--- UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Justin', `LastName` = 'Lin' WHERE (`Title` = 'Tokyo Drift');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Michael', `LastName` = 'Bay' WHERE (`Title` = 'Armageddon ');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Michael', `LastName` = 'Bay' WHERE (`Title` = 'Bad Boys');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Christopher', `LastName` = 'Nolan' WHERE (`Title` = 'Interstellar');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Oran', `LastName` = 'Peli' WHERE (`Title` = 'Paranormal Activity');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Keenan', `LastName` = 'Wayans' WHERE (`Title` = 'Scary Movie 1');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Keenan', `LastName` = 'Wayans' WHERE (`Title` = 'Scary Movie 2');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Vicky', `LastName` = 'Jenson' WHERE (`Title` = 'Shrek');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Sam', `LastName` = 'Raimi' WHERE (`Title` = 'Spider-Man 3');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Tobe', `LastName` = 'Hooper' WHERE (`Title` = 'Texas Chainsaw Massacre');
+UPDATE `guidedInquiry`.`hard` SET `FirstName` = 'Justin', `LastName` = 'Lin' WHERE (`Title` = 'Tokyo Drift');
 
--- ALTER TABLE Hard 
--- 	ADD FullName varchar(40) NOT NULL
--- 		AFTER LastName;
+ALTER TABLE Hard 
+	ADD FullName varchar(40) NOT NULL
+		AFTER LastName;
 
--- UPDATE Hard SET FullName = CONCAT(FirstName, ' ', LastName)
+UPDATE Hard SET FullName = CONCAT(FirstName, ' ', LastName)
 
--- SELECT * FROM guidedInquiry.hard
--- ORDER BY `FullName` ASC;
+SELECT * FROM guidedInquiry.hard
+ORDER BY `FullName` ASC;
 
